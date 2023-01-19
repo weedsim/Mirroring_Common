7b00c7d6a22d97abf17e2da74ec29e9f54a99185
charoon <wjswndud53@gmail.com>
2023-01-19 15:14:02 +0900

    secret key 변경 key값을 256bit이상으로 해야함

--- back-end/liargame/src/main/java/com/sixsense/liargame/common/auth/JwtProperties.java
+++ back-end/liargame/src/main/java/com/sixsense/liargame/common/auth/JwtProperties.java
@@ -1,7 +1,7 @@
 package com.sixsense.liargame.common.auth;
 
 public interface JwtProperties {
-    String SECRET = "Sm0aDWgZH3O9kfS6w5yBhVSPIr1OSeVD"; // 우리 서버만 알고 있는 비밀값
+    String SECRET = "zPT5HqJxqJPsetlB6rnsQGCcoWP5T2M9sdjfjaslkjfle"; // 우리 서버만 알고 있는 비밀값
     int ACCESS_TOKEN_TIME =  1000 * 60 * 30;
     int REFRESH_TOKEN_TIME = 6 * 24 * 60 * 60 * 1000;
 }
